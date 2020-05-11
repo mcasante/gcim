e4649c20d48351c8bd9a5346ec72a59758d671fa
Matheus Casante <math.casante@gmail.com>
2020-05-11 02:18:27 -0300

    Adjust section height, add fake routing and images layout

--- assets/script.js
+++ assets/script.js
@@ -1,31 +1,48 @@
 const selectAll = query => Array.from(document.querySelectorAll(query))
 
-function setThumbs(sections, holder) {
+function setSections(sections, holder) {
+    const routes = []
     sections.forEach(s => {
         holder.appendChild(
             createThumb(s)
         )
+        routes.push(s.getAttribute('data-slug'))
     })
+    return routes
 }
 
 function createThumb(section) {
     const title = section.getAttribute('data-title')
     const div = document.createElement('div')
+    div.setAttribute('data-link', section.getAttribute('data-slug'))
     div.classList.add('thumb')
     div.innerHTML = `<span>${title}</span>`
 
     div.addEventListener('click', function() {
-        selectAll('.thumb').forEach(t => t.classList.remove('active'))
-        section.scrollIntoView({behavior: "smooth"})
-        this.classList.add('active')
+        setSelected(this)
+        section.scrollIntoView({behavior: 'smooth'})
     })
-
+    
     return div
+}
+
+function setSelected(selected) {
+    selectAll('.thumb').forEach(t => t.classList.remove('active'))
+    selected.classList.add('active')
+    location.hash = selected.getAttribute('data-link')
 }
 
 window.addEventListener('load', function(){
     const sections = selectAll('#main section')
     const thumbHolder = document.querySelector('#thumbnails .thumbholder')
     
-    setThumbs(sections, thumbHolder)
+    const routes = setSections(sections, thumbHolder)
+
+    const theHash = location.hash.substr(1);
+
+    if(routes.includes(theHash)) {
+        document.querySelector(`[data-link="${theHash}"]`).click()
+    } else {
+        document.querySelector('.thumb').click()
+    }
 })
